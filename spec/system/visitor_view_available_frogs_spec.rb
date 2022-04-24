613e67043cec8a601b76e4ecabfb320d36f55ceb
minccia <lusyvasilva@gmail.com>
2022-04-24 17:02:49 -0300

    Fixed some portuguese words to english only

--- spec/system/visitor_view_available_frogs_spec.rb
+++ spec/system/visitor_view_available_frogs_spec.rb
@@ -6,14 +6,14 @@ describe 'I have seen a frog' do
   it 'Sucessfully' do
     Frog.create!(
       name: "Rebeca",
-      scientific_name: "Sapo normal",
+      scientific_name: "Common frog",
       age: 5,
       on_sale: true
     )
 
     Frog.create!(
         name: "Daniel",
-        scientific_name: "Sapo venenoso",
+        scientific_name: "Poisonous frog",
         age: 3,
         on_sale: false
       )
@@ -21,15 +21,15 @@ describe 'I have seen a frog' do
     visit root_path
     click_on 'Frog Gallery'
 
-    expect(page).to_not have_content('Não há nenhum sapo ainda')
+    expect(page).to_not have_content('There are no frogs yet')
     expect(page).to have_css('.frog', count:2)
-    expect(page).to have_content('Sapo venenoso')
-    expect(page).to have_content('Sapo normal')
+    expect(page).to have_content('Poisonous frog')
+    expect(page).to have_content('Common frog')
   end
 
   it 'Unless there are not any frogs' do
     visit root_path
     click_on 'Frog Gallery'
-    expect(page).to have_content('Não há nenhum sapo ainda')
+    expect(page).to have_content('There are no frogs yet')
   end
 end
